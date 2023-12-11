1490805def7e2fd7ff8d748b5a9bdfcf6b865374
asterodeia <d_wang_890227@outlook.com>
2023-12-11 16:33:45 +0800

    saving point 11

--- script.js
+++ script.js
@@ -17,6 +17,76 @@ const cadenceField = form.querySelector("#workout-cadence");
 
 const _list = document.querySelector(".workout-list");
 
+class Form {
+  #parent;
+  #fieldIds = {
+    type: "#workout-type",
+    data: {
+      distance: "#workout-distance",
+      duration: "#workout-duration",
+      cadence: "#workout-cadence",
+      elevGain: "#workout-elev-gain",
+    },
+  };
+
+  constructor(parentNode) {
+    this.#parent = parentNode;
+  }
+
+  get type() {
+    return this.#parent.querySelector(this.#fieldIds.type).value.toLowerCase();
+  }
+  _rawData() {
+    const data = {};
+    Object.keys(this.#fieldIds.data).forEach(key => {
+      data[key] = this.#parent.querySelector(this.#fieldIds.data[key]).value;
+    });
+
+    return { this[type], data };
+  }
+
+  static _checkPositive(data) {
+    return Object.values(data).every(value => value > 0);
+  }
+
+  static _checkValid(data) {
+    return Object.values(data).every(value => {
+      return value.trim() !== "" && !isNaN(Number(value));
+    });
+  }
+
+  getParsedData() {
+    const { type, data: rawData } = this._rawData();
+    delete rawData[type === "running" ? "elevGain" : "cadence"];
+
+    const dataValues = Object.values(rawData);
+
+    if (!Form._checkValid(dataValues) || !Form._checkPositive(dataValues))
+      throw Error("values must be positive numbers");
+
+    const data = {};
+    Object.keys(rawData).forEach(key => (data[key] = Number(rawData[key])));
+
+    return { type, data };
+  }
+
+  show() {
+    this.#parent.classList.remove("hidden");
+  }
+
+  hide() {
+    this.#parent.classList.add("hidden");
+  }
+
+  _clear() {
+    this.#parent.reset();
+  }
+
+  _changeFormType() {}
+}
+
+const frm = new Form(form);
+
 const _getFormData = form => {
   let type;
   const fieldIds = [
